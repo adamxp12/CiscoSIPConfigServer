c06595d7094843205a44805fd7eb05f1d8c11d27
Adam Blunt <adamxp12@adamxp12.com>
2017-03-01 00:00:55 +0000

    Hello worldv2, aka I forgot whats in this commit

--- server.js
+++ server.js
@@ -7,16 +7,28 @@
 var express = require('express'),
     app = express(),
     clear = require("cli-clear"),
-    package = require('./package.json');
+    package = require('./package.json'),
+    xss = require('xss'),
+    shortid = require('shortid'),
+    helmet = require('helmet');
 
 // Some varibles
 var port = 3000;
+
+// Express setup
+app.use(express.static('public'));
+app.use(helmet({
+  hidePoweredBy: false
+}));
+
+// Init finished time to do some routes
 
 app.get('/', function (req, res) {
     res.send(package.name + " v" + package.version)
 })
 
 
+// Routes over so lets start the server
 app.listen(port, function () {
     clear()
     console.log(package.name + " v" + package.version)
