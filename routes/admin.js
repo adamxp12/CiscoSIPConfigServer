47baa35c7b6df6198ac3cfcc8b72a09a7758e5db
Adam Blunt <adamxp12@adamxp12.com>
2017-03-05 22:57:54 +0000

    Firmware page

--- routes/admin.js
+++ routes/admin.js
@@ -5,19 +5,29 @@ var express = require('express'),
 
 var header = fs.readFileSync("./inc/header.inc", "utf8", function(err, data) { if (err) throw err; });
 var footer = fs.readFileSync("./inc/footer.inc", "utf8", function(err, data) { if (err) throw err; });
+var adminmenu = fs.readFileSync("./inc/adminmenu.inc", "utf8", function(err, data) { if (err) throw err; });
 var dashboardinc = fs.readFileSync("./inc/dashboard.inc", "utf8", function(err, data) { if (err) throw err; });
+var firmwareinc = fs.readFileSync("./inc/firmware.inc", "utf8", function(err, data) { if (err) throw err; });
+
 
 router.get('*', function(req, res, next) {
     if(req.session.user){
-        req.page = header;
+        req.page = header+adminmenu.replace("{username}", req.session.user);
         next();
     } else {
         res.redirect('/');
     }
 })
 
+// Main dashboard
 router.get('/', function(req, res, next) {
-    req.page = req.page + dashboardinc.replace("{username}", req.session.user)
+    req.page = req.page + dashboardinc
+    next()
+})
+
+// Firmware dashboard
+router.get('/firmware', function(req, res, next) {
+    req.page = req.page + firmwareinc
     next()
 })
 
